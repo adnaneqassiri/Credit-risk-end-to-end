60941080a94bfe9b6d5bba204e892ef9f2d2fe15
adnaneqassiri <adnaneqassiri@gmail.com>
2026-04-14 23:08:24 +0100

    update

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
-TRAIN_DATA_DIR = DATA_DIR / "train_data"
+TRAIN_DATA_DIR = DATA_DIR / "gold"
 ARTIFACTS_DIR = ROOT_DIR / "artifacts"
 SUBMISSIONS_DIR = DATA_DIR / "submissions"
 MODEL_PATH = ARTIFACTS_DIR / "lgbm_model.pkl"
